89df461b197693870f32a8ebe82005a8e99057f5
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2022-05-15 22:08:41 +0200

    adding basic route to test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use('/api/users', userRoutes)
 app.use('/api/urls', urlRoutes )
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Hello World!')
   })
 app.use(errorHandler);

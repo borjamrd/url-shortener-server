c794063561bba39c23d8d697e45043b5af6d48cd
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2022-12-19 20:40:34 +0100

    updating cors

--- server.js
+++ server.js
@@ -3,22 +3,24 @@ const dotenv = require('dotenv')
 const userRoutes = require('./routes/userRoutes')
 const urlRoutes = require('./routes/urlRoutes')
 const connectDB = require('./config/db')
-const {notFound, errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 
 dotenv.config()
 connectDB()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRoutes)
-app.use('/api/urls', urlRoutes )
+app.use('/api/urls', urlRoutes)
 app.get('/test', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 app.use(errorHandler);
 app.use(notFound);
 
-const PORT =  process.env.PORT || 8001
+const PORT = process.env.PORT || 8001
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
